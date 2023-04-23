77fbea8f25cbed5d46fb90a6b978c138ed64c800
Pranay <pranaydwivedi32@gmail.com>
2023-04-23 11:07:59 +0530

    Done higlhted
    Done highligting

--- src/components/Layout/HeaderCartButton/HeaderCartButton.component.jsx
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useContext } from "react";
 import { CartContext } from "../../../Contexts/Cart.context";
 import CartIcon from "../../Cart/CartIcon";
@@ -6,12 +8,26 @@ import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
   const { items } = useContext(CartContext);
+  const [buttonHiglighted, setButtonHighlighted] = useState(false);
   const numberOfCartItems = items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
 
+  const buttonClasses = ` ${classes.button} ${
+    buttonHiglighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length > 0) setButtonHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={() => props.openModal()}>
+    <button className={buttonClasses} onClick={() => props.openModal()}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
